45457720f559aa8008212b6cfb9944154401d4e0
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-09 21:13:49 +0700

    Setup knexfile

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: process.env.DB_SERVER,
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+  staging: {
+    client: process.env.DB_SERVER,
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+
+  production: {
+    client: process.env.DB_SERVER,
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  }
+
+};
