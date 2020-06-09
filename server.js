0e3187dca6c6efab97693409a5da9ff495e42ef3
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-09 20:36:19 +0700

    Base code server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express')
+const app               = express()
+const bodyParser          = require('body-parser')
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT;
+
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
