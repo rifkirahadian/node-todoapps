df604cc4391b45fc185499382fab6ab2d8063ad2
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-10 22:29:15 +0700

    Migration: tasks

---
+++ database/migrations/20200610222024_create_task_table.js
@@ -0,0 +1,19 @@
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('tasks', function (table) {
+      table.increments('id')
+      table.integer('user_id').unsigned().notNullable();
+      table.foreign('user_id').references('id').inTable('users')
+      table.string('name', 150).notNullable()
+      table.string('place', 150)
+      table.date('date').notNullable()
+      table.time('start_time').notNullable()
+      table.time('end_time')
+      table.timestamps()
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('tasks')
+};
