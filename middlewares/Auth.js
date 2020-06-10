58408d49575aa36c75c9a62384e7a92e534ba98c
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-10 22:39:01 +0700

    Middleware: Auth

---
+++ middlewares/Auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const responser = require('../modules/responser')
+
+module.exports = (authRoutes) => {
+  authRoutes.use((req, res, next) => {
+    // check header parameters for token
+    let token = req.headers['Authorization']
+
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, 'superSecret', function (err, decoded) {
+        if (err) {
+          return responser.errorResponseStatus(res, 401, 'Failed to authenticate token')
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.user = decoded
+          next()
+        }
+      })
+
+    } else {
+      // if there is no token
+      // return an error
+      return responser.errorResponseStatus(res, 401, 'No token provided.')
+    }
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const AuthController = require('../controllers/AuthController')
-
 const validator = require('../modules/validator')
 
 const authController = new AuthController
 
 module.exports = (app, express) => {
   const apiRoutes = express.Router()
+  const authRoutes = express.Router()
+
+  //middleware
+  require('../middlewares/Auth')(authRoutes)
 
   apiRoutes.post('/register',validator.register, authController.register)
   apiRoutes.post('/login',validator.login, authController.login)
 
   app.use('/api', apiRoutes)
+  app.use('/api', authRoutes)
 }
